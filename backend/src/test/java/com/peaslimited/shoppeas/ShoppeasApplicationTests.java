90fa3a2648ee53a2803d20ee9771653709b9402d
Rachel Tan <racheltanmz@gmail.com>
2024-08-30 14:44:51 +0800

    added backend

---
+++ backend/src/test/java/com/peaslimited/shoppeas/ShoppeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peaslimited.shoppeas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
