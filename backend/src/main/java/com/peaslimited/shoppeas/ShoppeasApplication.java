8167780dd6603debb7123ac4a21bfd8dea2dab79
Rachel Tan <racheltanmz@gmail.com>
2024-09-25 10:23:08 +0800

    updated backend to spring boot

---
+++ backend/src/main/java/com/peaslimited/shoppeas/ShoppeasApplication.java
@@ -0,0 +1,13 @@
+package com.peaslimited.shoppeas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppeasApplication.class, args);
+	}
+
+}
