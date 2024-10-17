fd966dfc80e8be61ab30143a2eaab8b6c252d5b1
Jun Chao <jc1jc2jc1jc1@gmail.com>
2024-10-17 10:39:34 +0800

    added CartService and WholesalerService files

---
+++ frontend/src/service/CartService.js
@@ -0,0 +1,23 @@
+import apiClient from "../api/apiClient";
+import { REACT_APP_BACKEND_PRODUCT } from '@env';
+
+// API calls for customers
+export const getCart = async (uid) => {
+    // const endpoint = "/products/all"; 
+    try {
+        const response = await apiClient.get(`${REACT_APP_BACKEND_AUTH}/${endpoint}`, {
+            headers: {
+                Authorization: `Bearer ${uid}`,
+                Accept: "application/json",
+            },
+        });
+        return response.data;
+
+    } catch (error) {
+        console.error("Error config:", JSON.stringify(error.config, null, 2));
+        throw error;
+    }
+};
+
+
+
