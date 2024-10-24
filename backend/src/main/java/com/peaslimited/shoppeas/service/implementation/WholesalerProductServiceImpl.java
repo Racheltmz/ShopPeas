80583e23aa0836df2ea788ce75005334b9efb97f
Rachel Tan <racheltanmz@gmail.com>
2024-10-24 12:33:26 +0800

    updated file

--- backend/src/main/java/com/peaslimited/shoppeas/service/implementation/WholesalerProductServiceImpl.java
+++ backend/src/main/java/com/peaslimited/shoppeas/service/implementation/WholesalerProductServiceImpl.java
@@ -38,8 +38,8 @@ public class WholesalerProductServiceImpl implements WholesalerProductService {
     }
 
     @Override
-    public void updateWholesalerProduct(String swpid, Map<String, Object> updates) throws ExecutionException, InterruptedException {
-        wholesalerProductRepository.updateWholesalerProduct(swpid, updates);
+    public void updateWholesalerProduct(String swp_id, Map<String, Object> updates) throws ExecutionException, InterruptedException {
+        wholesalerProductRepository.updateWholesalerProduct(swp_id, updates);
     }
 
     @Override
@@ -49,14 +49,13 @@ public class WholesalerProductServiceImpl implements WholesalerProductService {
     }
 
     @Override
-    public void deleteWholesalerProduct(String swpid) {
-        wholesalerProductRepository.deleteWholesalerProduct(swpid);
+    public void deleteWholesalerProduct(String swp_id) {
+        wholesalerProductRepository.deleteWholesalerProduct(swp_id);
     }
 
     @Override
-    public String getWholesalerProductName(String swpid) throws ExecutionException, InterruptedException
-    {
-        return wholesalerProductRepository.getWholesalerProductName(swpid);
+    public String getWholesalerProductName(String swp_id) throws ExecutionException, InterruptedException {
+        return wholesalerProductRepository.getWholesalerProductName(swp_id);
     }
 
 
